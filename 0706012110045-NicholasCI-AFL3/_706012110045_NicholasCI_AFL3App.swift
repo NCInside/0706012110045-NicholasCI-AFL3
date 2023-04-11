88fb2ecaa99a1b4c795913c7f19f894fcd61d7fa
NCInside <nchristian02@student.ciputra.ac.id>
2023-04-11 08:19:51 +0700

    Initial Commit

---
+++ 0706012110045-NicholasCI-AFL3/_706012110045_NicholasCI_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110045_NicholasCI_AFL3App.swift
+//  0706012110045-NicholasCI-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110045_NicholasCI_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
